d894ad0f33e43e951b77aa166eee99691d9001ff
Mohammed Hashim Anwar <84743105+hashimnwr@users.noreply.github.com>
2023-02-16 20:58:06 +0300

    Number Guessing Game version 1.0

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+
+end_game = False
+NUMBER = random.randint(1, 100)
+print("Welcome to number guessing game...\nI'm thinking a number between 1 and 100.")
+difficulty = input("Choose difficulty level, 'easy' or 'hard' : ").lower()
+
+if difficulty == 'hard':
+  lives = 5
+elif difficulty == 'easy':
+  lives = 10
+else:
+  print("input error, please check the spelling...")
+  end_game = True
+
+def deduct():
+  global lives
+  lives -= 1
+  
+while not end_game:
+  guess = input("Make a guess: ")
+  guess = int(guess)
+  diff = abs(guess - NUMBER)
+  if guess == NUMBER:
+    print(f"You guessed it right, with {lives} lives remaining...")
+    end_game = True
+  else:
+    deduct()
+    print(lives)
+    if lives == 0:
+      end_game = True
+      print(f"Answer = {NUMBER}")
+    else:
+      if guess > NUMBER:
+        print("High")
+        if diff < 3:
+          print("too close")
+      elif guess < NUMBER:
+        print("Low")
+        if diff < 3:
+          print("too close")
