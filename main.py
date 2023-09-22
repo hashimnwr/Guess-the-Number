5d0e27917d3bd380d0b3c031bf73b832fc4cd30a
Mohammed Hashim Anwar <84743105+hashimnwr@users.noreply.github.com>
2023-09-22 12:40:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,36 +6,32 @@ print("Welcome to number guessing game...\nI'm thinking a number between 1 and 1
 difficulty = input("Choose difficulty level, 'easy' or 'hard' : ").lower()
 
 if difficulty == 'hard':
-  lives = 5
+    lives = 5
 elif difficulty == 'easy':
-  lives = 10
+    lives = 10
 else:
-  print("input error, please check the spelling...")
-  end_game = True
-
-def deduct():
-  global lives
-  lives -= 1
+    print("input error, please check the spelling...")
+    end_game = True
   
 while not end_game:
-  guess = input("Make a guess: ")
-  guess = int(guess)
-  diff = abs(guess - NUMBER)
-  if guess == NUMBER:
-    print(f"You guessed it right, with {lives} lives remaining...")
-    end_game = True
-  else:
-    deduct()
-    print(lives)
-    if lives == 0:
-      end_game = True
-      print(f"Answer = {NUMBER}")
+    guess = input("Make a guess: ")
+    guess = int(guess)
+    diff = abs(guess - NUMBER)
+    if guess == NUMBER:
+        print(f"You guessed it right, with {lives} lives remaining...")
+        end_game = True
     else:
-      if guess > NUMBER:
-        print("High")
-        if diff < 3:
-          print("too close")
-      elif guess < NUMBER:
-        print("Low")
-        if diff < 3:
-          print("too close")
+        lives -=1
+        print(f'You have {lives} attempts left')
+        if lives == 0:
+            end_game = True
+            print(f"Answer = {NUMBER}")
+        else:
+            if guess > NUMBER:
+                print("High")
+                if diff < 3:
+                    print("too close")
+            elif guess < NUMBER:
+                print("Low")
+                if diff < 3:
+                    print("too close")
